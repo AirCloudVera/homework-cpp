90fed3c5b47597133352253ed4a407c91e163e56
AirCloudVera <verakondurina@gmail.com>
2025-09-20 15:57:31 +0000

    Update task01

--- task_01/src/main.cpp
+++ task_01/src/main.cpp
@@ -64,9 +64,12 @@ int b = *(pa + 1);  // значение
 int* Fibb(int size) {
   int* arr = new int[size];
   arr[0] = 1;
+  // *arr = 1;
   arr[1] = 1;
+  // *(arr + 1) = 1;
   for (int i = 2; i < size + 2; ++i) {
-    arr[i] = *(arr + (i - 2)) + *(arr + i - 1);
+    // arr[i] = *(arr + (i - 2)) + *(arr + i - 1);
+    arr[i] = arr[i - 2] + arr[i - 1];
   }
   return arr;
 }
